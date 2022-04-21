d22fee26c9c9eb0321913326e992b85fa72d7fbe
Jitu Behera <jitu.b@simformsolutions.com>
2022-04-21 12:50:45 +0530

    Added unit test

---
+++ tests/Bidder.Activities.Api.UnitTests/RegistrationStatusValidatorTest.cs
@@ -0,0 +1,38 @@
+﻿using Bidder.Activities.Domain.Entities;
+using Bidder.Activities.Domain.Validators.Fluent;
+using FluentAssertions;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace Bidder.Activities.Api.UnitTests
+{
+    public class RegistrationStatusValidatorTest
+    {
+        private readonly RegistrationStatusValidator _registrationStatusValidatorTest;
+
+        public RegistrationStatusValidatorTest()
+        {
+            _registrationStatusValidatorTest = new RegistrationStatusValidator();
+        }
+        [Theory]
+        [InlineData(11)]
+        public void When_AuctionId_Is_Positive_Number_Should_Not_Have_Any_ValidationError(long auctionId)
+        {
+            var registrationStatusDomain = new RegistrationStatusDomain(_registrationStatusValidatorTest, auctionId);
+            var result = _registrationStatusValidatorTest.TestValidate(registrationStatusDomain);
+            result.IsValid.Should().Be(true);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void When_AuctionId_Is_Negative_Number_Should_Not_Have_Any_ValidationError(long auctionId)
+        {
+            var registrationStatusDomain = new RegistrationStatusDomain(_registrationStatusValidatorTest, auctionId);
+            var result = _registrationStatusValidatorTest.TestValidate(registrationStatusDomain);
+            result.IsValid.Should().Be(false);
+        }
+
+
+    }
+}
